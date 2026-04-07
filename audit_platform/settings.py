604abc84d35065c688d5ebd37653f7760395439e
Rodrigo Cortes <carlos.cortescs@udlap.mx>
2026-04-06 19:51:37 -0600

    fix allowed hosts railway

--- audit_platform/settings.py
+++ audit_platform/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-96i426z&$i(%nz)2-k34f9-bd44cwa@l=qax%_%^$r)2xi$*l!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost","*"]
 
 
 # Application definition
